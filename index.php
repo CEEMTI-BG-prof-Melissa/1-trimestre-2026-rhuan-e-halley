31e83cbb5cf4169883f597a9c34aef830d7953b2
rhuanopmachado-source <rhuan.opmachado@aluno.edu.es.gov.br>
2026-06-24 11:47:57 -0300

    aula02

---
+++ index.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+
+
+    <html lang="pt-br">
+        <meta charset="utf-8">
+    <head>
+        <title>Aula 02 PHP </title>
+    </head>
+    <body>
+        <?php
+        $nome1 = "Rhuan";
+        $idade1 = 17;
+        $nome2 = "Halley";
+        $idade2 = 17;
+        echo "Nomes " .$nome1 ." " .$nome2 ." Idades: " .$idade1 ." e " .$idade2 . " anos";
+        ?>
+    </body>
+</html>
+
